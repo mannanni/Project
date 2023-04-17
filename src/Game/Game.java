beb2793c4fced83f81eff08fd560e288286e4638
Annie <123340925+mannanni@users.noreply.github.com>
2023-04-16 21:57:26 -0400

    Update Game.java

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -14,31 +14,79 @@ package Game;
  * @author Swati Sharma
  * @author Ajay Bookal
  */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A simple card game where the goal is to draw the highest card from a shuffled deck.
+ */
 public class Game {
 
-    private static Card draw() {
-        
-        return null;
-        
-    }
+    private final List<Card> deck;
 
-    private static int deckSize() {
-        
-        return 0;
-        
-    }
-
-    private static void main(String[] args) {
-    }
-    
+    /**
+     * Creates a new Game object with a shuffled deck of 52 standard playing cards.
+     */
     public Game() {
+        deck = new ArrayList<>();
+        for (Rank rank : Rank.values()) {
+            for (Suit suit : Suit.values()) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+        Collections.shuffle(deck);
     }
 
-
-    public void setUp() throws Exception {
+    /**
+     * Draws the top card from the deck and removes it from the deck.
+     *
+     * @return the Card object representing the top card of the deck.
+     * @throws IllegalStateException if the deck is empty.
+     */
+    public Card draw() {
+        if (deck.isEmpty()) {
+            throw new IllegalStateException("Deck is empty");
+        }
+        return deck.remove(0);
     }
-    
- 
+
+    /**
+     * Returns the number of cards remaining in the deck.
+     *
+     * @return the number of cards remaining in the deck.
+     */
+    public int deckSize() {
+        return deck.size();
+    }
+
+    /**
+     * Plays a game of cards where each player draws one card and the player with the highest card wins.
+     * In case of a tie, the game is a draw.
+     *
+     * @param numPlayers the number of players in the game.
+     * @return the index of the winning player (1-indexed), or 0 for a draw.
+     * @throws IllegalArgumentException if numPlayers is less than 2.
+     */
+    public int play(int numPlayers) {
+        if (numPlayers < 2) {
+            throw new IllegalArgumentException("Must have at least 2 players");
+        }
+
+        int winningPlayer = 0;
+        int highestRank = -1;
+        for (int i = 0; i < numPlayers; i++) {
+            Card card = draw();
+            if (card.getRank().getValue() > highestRank) {
+                highestRank = card.getRank().getValue();
+                winningPlayer = i + 1;
+            } else if (card.getRank().getValue() == highestRank) {
+                winningPlayer = 0;
+            }
+        }
+        return winningPlayer;
+    }
+
 
     /**
      * Test of draw method, of class Game.
